ef4de6f59c1c2abe3ec834fe8ab43b4704668065
OMI786I <sohrawardy1998@gmail.com>
2024-04-30 13:34:25 +0600

    added active and inactive condition

--- B9A6-Retro-Forum-main/code.js
+++ B9A6-Retro-Forum-main/code.js
@@ -21,56 +21,109 @@ const displayData = (data) => {
   data.forEach((element) => {
     jsonData.push(element);
     const dataDiv = document.createElement("div");
-
-    dataDiv.innerHTML = `
-    <div class="hero mt-3 rounded-xl bg-base-200" ${element.category}>
-    <div class="hero-content flex-row lg:flex-row items-start">
-   
-    <div class ="avatar online" id="avatar-container"> 
-
-    <div class= "  w-[60px] rounded-lg shadow-2xl">
-    
-    <img src="${element.image}" class="" />
-    </div>
-    
-    
-    </div>
-     
-      <div>
-
-      <div class = "flex gap-3">
-      <h1>#<span id = "categoryName">${element.category}</span></h1>
-      <h1>Author : ${element.author.name}</h1>
-      </div>
+    if (element.isActive === true) {
+      dataDiv.innerHTML = `
+      <div class="hero mt-3 rounded-xl bg-base-200" ${element.category}>
+      <div class="hero-content flex-row lg:flex-row items-start">
       
-        <h1 class="text-xl font-bold">${element.title}</h1>
-        <p class="py-6">${element.description}</p>
-        <div class ="flex justify-between">
-        <div class = "flex items-center gap-1">
-        <div class ="flex items-center gap-1">
-        <i class="fa-regular fa-message"></i>
-        <p>${element.comment_count}</p>
-        </div>
       
-        <div class = "flex items-center gap-1">
-        <i class="fa-regular fa-eye"></i>
-        <p>${element.view_count}</p>
-        </div>
-        <div class = "flex items-center gap-1">
-        <i class="fa-regular fa-clock"></i>
-        <p>${element.posted_time}</p>
-        
-        </div>
-        </div>
-        <div class = "border rounded-full p-3 bg-[#10b981] cursor-pointer">
-        <i class="fa-regular fa-envelope-open text-white" onclick="clickList('${element.id}')"  href="#" ></i>
-        </div>
-        </div>
-      </div>
-    </div>
+  <div id ="avatar-container">
+  <div class = "avatar online"  > 
+  <div class= "  w-[60px] rounded-lg shadow-2xl">
+  <img src="${element.image}" class="" />
   </div>
+  </div>
+  </div>     
+  
+  
+  
+        <div>
+  
+        <div class = "flex gap-3">
+        <h1>#<span id = "categoryName">${element.category}</span></h1>
+        <h1>Author : ${element.author.name}</h1>
+        </div>
+        
+          <h1 class="text-xl font-bold">${element.title}</h1>
+          <p class="py-6">${element.description}</p>
+          <div class ="flex justify-between">
+          <div class = "flex items-center gap-1">
+          <div class ="flex items-center gap-1">
+          <i class="fa-regular fa-message"></i>
+          <p>${element.comment_count}</p>
+          </div>
+        
+          <div class = "flex items-center gap-1">
+          <i class="fa-regular fa-eye"></i>
+          <p>${element.view_count}</p>
+          </div>
+          <div class = "flex items-center gap-1">
+          <i class="fa-regular fa-clock"></i>
+          <p>${element.posted_time}</p>
+          
+          </div>
+          </div>
+          <div class = "border rounded-full p-3 bg-[#10b981] cursor-pointer">
+          <i class="fa-regular fa-envelope-open text-white" onclick="clickList('${element.id}')"  href="#" ></i>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+   
+          `;
+    } else {
+      dataDiv.innerHTML = `
+      <div class="hero mt-3 rounded-xl bg-base-200" ${element.category}>
+      <div class="hero-content flex-row lg:flex-row items-start">
       
-        `;
+      
+  <div id ="avatar-container">
+  <div class = "avatar offline"  > 
+  <div class= "  w-[60px] rounded-lg shadow-2xl">
+  <img src="${element.image}" class="" />
+  </div>
+  </div>
+  </div>     
+  
+  
+  
+        <div>
+  
+        <div class = "flex gap-3">
+        <h1>#<span id = "categoryName">${element.category}</span></h1>
+        <h1>Author : ${element.author.name}</h1>
+        </div>
+        
+          <h1 class="text-xl font-bold">${element.title}</h1>
+          <p class="py-6">${element.description}</p>
+          <div class ="flex justify-between">
+          <div class = "flex items-center gap-1">
+          <div class ="flex items-center gap-1">
+          <i class="fa-regular fa-message"></i>
+          <p>${element.comment_count}</p>
+          </div>
+        
+          <div class = "flex items-center gap-1">
+          <i class="fa-regular fa-eye"></i>
+          <p>${element.view_count}</p>
+          </div>
+          <div class = "flex items-center gap-1">
+          <i class="fa-regular fa-clock"></i>
+          <p>${element.posted_time}</p>
+          
+          </div>
+          </div>
+          <div class = "border rounded-full p-3 bg-[#10b981] cursor-pointer">
+          <i class="fa-regular fa-envelope-open text-white" onclick="clickList('${element.id}')"  href="#" ></i>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+   
+          `;
+    }
 
     dataContainer.appendChild(dataDiv);
   });
@@ -86,11 +139,10 @@ loadData();
 
 const clickList = async (id) => {
   for (const iterator of jsonData) {
-    console.log(iterator);
-    if (iterator.id == id) {
-      const container = document.getElementById("selected-data");
-      const div = document.createElement("div");
+    const container = document.getElementById("selected-data");
+    const div = document.createElement("div");
 
+    if (iterator.id == id) {
       div.innerHTML = `
       <div class = " flex justify-between mb-3 bg-white p-4 rounded-xl">
       <h1 class="text-xl">${iterator.title}</h1>
@@ -103,3 +155,24 @@ const clickList = async (id) => {
     }
   }
 };
+
+/*
+const active = (id) => {
+  const container = document.getElementById("avatar-container");
+
+  for (const iterator of jsonData) {
+    const newDiv = document.createElement("div");
+    if (iterator.isActive == true) {
+      newDiv.innerHTML = `
+      <div class = "avatar online"  > 
+      <div class= "  w-[60px] rounded-lg shadow-2xl">
+      <img src="${iterator.image}" class="" />
+      </div>
+      </div>
+
+      `;
+      container.appendChild(newDiv);
+    }
+  }
+};
+*/
